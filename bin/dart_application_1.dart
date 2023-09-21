2452b9ea7958643244389fdd2d6909a6b1e00f0e
arunbabu33 <arun628287@gmail.com>
2023-09-21 13:08:47 +0530

    commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -98,3 +98,80 @@
 //   print(value);
 // }
 
+// day 6 sep 21 operators
+// arithmetic operators
+
+// Addition
+// multiplication
+// subtraction
+
+// void main() {
+// int a = 10, b = 20;
+// print("$a+$b =${a + b}");
+// print("$a-$b =${a - b}");
+// print("$a*$b =${a * b}");
+// print("$a%$b =${a % b}");
+// print("$a~/$b =${a ~/ b}");
+
+// assignment operators =, +=, -=, *=, /=, %=, ~/, etc
+
+// The value of   x will be updated on each line
+// dynamic x = 12, y = 5;
+// print("x=y -> ${x = y}");
+// print("x+=y -> ${x += y}");
+// print("x-=y -> ${x -= y}");
+// print("x*=y -> ${x *= y}");
+// print("x/=y -> ${x /= y}");
+// print("x%=y -> ${x %= y}");
+// print("x~/=y -> ${x ~/= y}");
+
+// relational operator >, <, >=, <=,==,!= etc
+
+// int i = 100, j = 50;
+// print("i>j ->${i > j}");
+// print("i<j ->${i < j}");
+// print("i>=j ->${i >= j}");
+// print("i<=j ->${i <= j}");
+// print("i==j ->${i == j}");
+// print("i!=j ->${i != j}");
+
+// logical operator && // !
+
+// String username = "admin";
+// String password = "abc";
+// print(username == "admin" && password == "abc");
+// print(username == "admin" || password == "abc");
+// print(!(username == "admin" && password == "abc"));
+
+// type test operator - is, is!
+
+// int k = 100;
+// print(k is int);
+// print(k is! String);
+
+// bitwise operator & | ^(xor) - to perform operations on binary values
+//  shift operators >>(right shift) <<(left shift)
+
+// *** conditional operator
+// 1. expression/condition ? true statement: false statement;
+// 2. expression1 ?? expression2
+// (if expression1 is null then expression 2 will be executed)
+
+//   var result = {username == "admin" && password == "abc"}
+//       ? "welcome user"
+//       : "incorrect username or password";
+//   print(result);
+// }
+
+void main() {
+//   String username = "admin";
+//   String password = "abc";
+//   var result = (username == "admin" && password == "abc")
+//       ? "welcome user"
+//       : "incorrect username or passsword";
+//   print(result);
+
+  // int x = 10, y = 20, z = 30;
+  // var out = x > y ? (x > z ? x : z) : (y > z ? y : z);
+  // print("$out is largest number");
+}
