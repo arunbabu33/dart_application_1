10a9456c8fb0a223d51d4ae8ee03a03d67f2fee1
arunbabu33 <arun628287@gmail.com>
2023-09-25 13:15:09 +0530

    commit

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -163,15 +163,214 @@
 //   print(result);
 // }
 
-void main() {
-//   String username = "admin";
-//   String password = "abc";
-//   var result = (username == "admin" && password == "abc")
-//       ? "welcome user"
-//       : "incorrect username or passsword";
-//   print(result);
+// void main() {
+// String username = "admin";
+// String password = "abc";
+// var result = (username == "admin" && password == "abc")
+//     ? "welcome user"
+//     : "incorrect username or passsword";
+// print(result);
 
-  // int x = 10, y = 20, z = 30;
-  // var out = x > y ? (x > z ? x : z) : (y > z ? y : z);
-  // print("$out is largest number");
+// int x = 10, y = 20, z = 30;
+// var out = x > y ? (x > z ? x : z) : (y > z ? y : z);
+// print("$out is largest number");
+
+// class A {
+//   void show() {
+//     print("show function");
+//   }
+
+//   void display() {
+//     print("display function");
+//   }
+
+//   void abc() {
+//     print("android");
+//   }
+// }
+
+// void main() {
+//   A obj = A();
+
+//   obj.show();
+//   obj.display();
+//   obj.abc();
+
+//   obj
+//     ..show()
+//     // ..display()        new way of calling
+//     ..abc();
+// }
+// }
+
+// control statements
+// import 'dart:io';
+
+// if else statement   ***************************
+// void main() {
+//   print("enter your age");
+//   int age = int.parse(stdin.readLineSync()!);
+
+//   if (age >= 18) {
+//     print("you are elgible for voting");
+//   } else {
+//     print("not elgible for voting");
+//   }
+// }
+
+// nested if else************
+// void main() {
+//   String username = "arun";
+//   String password = "1234";
+//   int otp = 102;
+
+//   if (username == "Arun" && password == 102) {
+//     print("login succesful");
+
+//     if (otp == 102) {
+//       print("otp verification succesfull");
+//     }
+//   } else {
+//     print("login failed");
+//   }
+// }
+
+// nested if else
+
+// void main(){
+//   if username =="admin" &&
+// }
+
+//  else if ladder
+// void main() {
+//   String size = "l"; //if (){}    else if (){}
+
+//   if (size == "xl") {
+//     print("shirt size is xl");
+//   } else if (size == "m") {
+//     print("shirt size is m");
+//   } else if (size == "xxl") {
+//     print("sirt size is s");
+//   } else if (size == "l") {
+//     print("shirt size is l");
+//   }
+// }
+
+// switch case
+
+// void main() {
+//   int size = 7;
+//   switch (size) {
+//     case 7:
+//       print("shoe size is 7");
+//     case 8:
+//       print("shoe size is 8");
+//     case 9:
+//       print("shoe size is 9");
+//     case 10:
+//       print("shoe size is 10");
+//   }
+// }
+
+// sep 25 iterators   or loops
+
+// void main() {
+// // for loop
+
+// // syntax for(initialisation ; condition checking;increment or decrement operator){}
+//   for (int a = 1; a <= 10; a++) {
+//     print(a);
+//   }
+// }
+
+// while loop/
+
+// void main() {
+//   // syntax initialisation
+//   // while(condition checking){
+//   // statements to be exhausted;
+//   // increment/decrement counter;}
+
+//   int a = 1;
+//   while (a <= 10) {
+//     print(a);
+//     a--;
+//   }
+// }
+
+// do while loop
+// syntax
+// initilalisation
+// d0{
+// statements to be exhausted
+// increment or decrement
+
+// }
+
+// void main() {
+//   int a = 1;
+//   do {
+//     print(a);
+//     a++;
+//   } while (a <= 100);
+// }
+
+// to print even numbers using for loop and if else
+
+// void main() {
+//   for (int a = 1; a <= 20; a++) {
+//     if (a % 2 == 0) {
+//       print(a);
+//     }
+//   }
+// }
+
+// to print odd nummbers
+
+// void main() {
+//   for (int a = 1; a <= 20; a++) {
+//     if (a % 2 == 1) {
+//       print(a);
+//     }
+//   }
+// }
+
+// sum of two natural numbers
+// void main() {
+//   int sum = 0;
+//   for (int i = 1; i <= 10; i++) {
+//     sum += i;
+//   }
+//   print("sum:$sum");
+// }
+
+//  sum of even and odd number in between 1 and 10
+// void main() {
+//   var sum = 0;
+//   for (int a = 1; a < 10; a++) {
+//     if (a % 2 == 1) {
+//       sum += a;
+//     }
+//   }
+//   print(sum);
+//   var abc = 0;
+//   for (int a = 1; a < 10; a++) {
+//     if (a % 2 == 0) {
+//       abc += a;
+//     }
+//   }
+//   print(abc);
+// }
+
+//  multiplication table
+
+import 'dart:io';
+
+void main() {
+  print("enter your value : ");
+  int num = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= 10; ++i) {
+    print("$num*$i = ${num * i}");
+  }
 }
